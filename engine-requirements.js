8333e1049df7c74906400c6135aae3a6db0dad66
RTypeByte <radzztypebyte@gmail.com>
2025-11-05 06:08:47 +0700

    MINOR UP

--- engine-requirements.js
+++ engine-requirements.js
@@ -1,10 +1,10 @@
 const major = parseInt(process.versions.node.split('.')[0], 10);
 
-if (major < 24) {
+if (major < 25) {
   console.error(
-    `\n This package requires Node.js 24+ to run reliably.\n` +
+    `\n This package requires Node.js 25+ to run reliably.\n` +
     `   You are using Node.js ${process.versions.node}.\n` +
-    `   Please upgrade to Node.js 20+ to proceed.\n`
+    `   Please upgrade to Node.js 25.0.0+ to proceed.\n`
   );
   process.exit(1);
 }
